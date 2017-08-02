87944ccee22a69998052ad6bc379bd50f0e888f6
alxbb <alxbb@mail.ru>
2017-08-02 17:29:47 +0300

    2017 08 02

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2611/Producer.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task26.task2611;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Producer implements Runnable {
+    private ConcurrentHashMap<String, String> map;
+
+    public Producer(ConcurrentHashMap<String, String> map) { this.map = map; }
+
+    @Override
+    public void run() {
+        Thread currentThread = Thread.currentThread();
+        int i = 1;
+        try {
+            while (true) {
+                map.put (String.valueOf(i), "Some text for " + i);
+                i++;
+                    Thread.sleep(500);
+                }
+        }catch (InterruptedException e) {
+            System.out.printf("[%s] thread was terminated", currentThread.getName() );
+        }
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task30.task3008;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+    public static String readString(){
+        String sOut;
+        while(true) {
+            try {
+                sOut = br.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return sOut;
+    }
+
+    public static int readInt(){
+        int iOut;
+        while(true) {
+            try {
+                iOut = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return iOut;}
+}
