0d5cdc7ec13fb42bb61c145f1653e0d5d8d1ad22
alxbb <alxbb@mail.ru>
2017-10-06 18:43:29 +0300

    NULL Object

--- 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3107;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttributeView;
 
 /*
 Null Object Pattern
@@ -9,6 +13,15 @@ public class Solution {
     private FileData fileData;
 
     public Solution(String pathToFile) {
+        try {
+            fileData = new ConcreteFileData(
+                    Files.isHidden(Paths.get(pathToFile)),
+                    Files.isExecutable(Paths.get(pathToFile)),
+                    Files.isDirectory(Paths.get(pathToFile)),
+                    Files.isWritable(Paths.get(pathToFile)));
+        } catch (Exception e) {
+            fileData = new NullFileData(e);
+        }
     }
 
     public FileData getFileData() {
